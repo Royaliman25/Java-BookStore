52a4a4eae389281dc79b5212c9ed9dfb3bd3f47f
Fitri <38976894+FitrianiGitana3@users.noreply.github.com>
2020-07-17 23:06:24 +0700

    Update

--- src/test/java/com/pluralsight/modul1/Module1_Task2_and_3_IT.java
+++ src/test/java/com/pluralsight/modul1/Module1_Task2_and_3_IT.java
@@ -14,3 +14,37 @@ public class Module1_Task2_and_3_IT {
                 "deleteBook", HttpServletRequest.class, HttpServletResponse.class);
     } catch (Exception e) {}
 }
+
+  @Test
+    public void _task3() throws Exception {
+       String tempID = "0";
+       String errorMsg = "private void deleteBook() does not exist in ControllerServlet";
+       assertNotNull(errorMsg, method);
+
+       ControllerServlet controllerServlet = PowerMockito.spy(new ControllerServlet());
+       boolean called_deleteBook = false;
+       HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
+       HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
+
+       try {
+         Mockito.when(request.getPathInfo()).thenReturn("/delete");
+         //PowerMockito.doNothing().when(controllerServlet, "deleteBook", request, response);
+         Mockito.when(request.getParameter("id")).thenReturn(tempID);
+       } catch (MethodNotFoundException e) {}
+
+       try {
+        controllerServlet.doGet(request, response);
+        try {
+           PowerMockito.verifyPrivate(controllerServlet)
+                       .invoke("deleteBook", request, response);
+           called_deleteBook = true;
+        } catch (Throwable e) {}
+       } catch (Exception e) {}
+
+       errorMsg = "After action \"" + "/delete" +
+                         "\", did not call deleteBook().";
+       assertTrue(errorMsg, called_deleteBook);
+    }
+}
+
+
