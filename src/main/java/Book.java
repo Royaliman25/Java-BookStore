74cc8e2ec5d5936f16928b6b6b39ace5b4cfb1a9
yosafat <67510154+yosafat64@users.noreply.github.com>
2020-07-17 23:37:38 +0700

    Update

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -18,3 +18,33 @@ public class Book {
 		this.author = author;
 		this.price = price;
 	}
+@Override
+	public String toString() {
+		return "(" + title + ", " + author + ", " + price + ")";
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+}
