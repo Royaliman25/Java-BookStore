2791083595d1090cb92a7178fac272027d3a05ba
Natanael <67508937+ncopa25@users.noreply.github.com>
2020-07-17 23:20:58 +0700

    Update

---
+++ src/test/java/com/pluralsight/modul3/Module3_Task1_thru_5_IT.java
@@ -0,0 +1,48 @@
+// package Modul 3
+package com.pluralsight.module3;
+
+// import pluralsight
+import com.pluralsight.*;
+
+//import static org.junit.Assert
+import static org.junit.Assert.*;
+
+//import java sql Connection
+import java.sql.Connection;
+
+//import java sql DriverManager
+import java.sql.DriverManager;
+
+//import java sql PreparedStatement
+import java.sql.PreparedStatement;
+
+//import Before Class
+import org.junit.BeforeClass;
+
+//import Test
+import org.junit.Test;
+
+//import Before
+import org.junit.Before;
+
+//import mockito
+import org.mockito.Mockito;
+
+//import runner
+import org.junit.runner.RunWith;
+
+//import Mockito api
+import org.powermock.api.mockito.PowerMockito;
+
+//import code classloder
+import org.powermock.core.classloader.annotations.PrepareForTest;
+
+//import modules
+import org.powermock.modules.junit4.PowerMockRunner;
+
+//import method java lang reflact
+import java.lang.reflect.Method;
+
+//import java input output
+import java.io.*;
+
